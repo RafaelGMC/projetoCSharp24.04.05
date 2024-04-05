022a6bc7b45ed77ab3cd22093a1a4aba930f1a89
rafaelGMC <rafael.g.de.m.carvalho@hotmail.com>
2024-04-05 20:24:45 -0300

    second commit

--- ProjetoLojaABC/frmLogin.cs
+++ ProjetoLojaABC/frmLogin.cs
@@ -7,13 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace ProjetoLojaABC
 {
     public partial class frmLogin : Form
     {
-        //variavel global
-      
+        //Criando variáveis para controle do menu
+        const int MF_BYCOMMAND = 0X400;
+        [DllImport("user32")]
+        static extern int RemoveMenu(IntPtr hMenu, int nPosition, int wFlags);
+        [DllImport("user32")]
+        static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+        [DllImport("user32")]
+        static extern int GetMenuItemCount(IntPtr hWnd);
+
         //método construtor
         public frmLogin()
         {
@@ -51,6 +59,13 @@ namespace ProjetoLojaABC
         private void btnSair_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            IntPtr hMenu = GetSystemMenu(this.Handle, false);
+            int MenuCount = GetMenuItemCount(hMenu) - 1;
+            RemoveMenu(hMenu, MenuCount, MF_BYCOMMAND);
         }
     }
 }
